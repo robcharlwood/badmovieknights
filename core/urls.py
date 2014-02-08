2cd9b0794ee65915e057e80f692472a99091c6c5
Rob Charlwood <robcharlwood@gmail.com>
2014-02-08 19:31:25 +0000

    Sorting core

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,14 @@
 # import django deps
 from django.conf.urls import url, patterns
+from django.conf import settings
 
-# import app deps
-from core.views import home_view
 
-# setup urls
-urlpatterns = patterns(
-    '',
-    url(r'^$', home_view, name='home'),
-)
+# handle 404s and 500s in development mode
+if settings.DEBUG:
+    urlpatterns = patterns(
+        '',
+        url(r'^500/$', 'django.views.generic.simple.direct_to_template', {
+            'template': 'core/500.html'}),
+        url(r'^404/$', 'django.views.generic.simple.direct_to_template', {
+            'template': 'core/404.html'}),
+    )
--- urls.py
+++ urls.py
@@ -1,6 +1,5 @@
 # import django deps
 from django.conf.urls import patterns, include, url
-from django.conf import settings
 
 # set up urls
 urlpatterns = patterns(
@@ -9,13 +8,3 @@ urlpatterns = patterns(
     url(r'^api/auth/', include(
         'rest_framework.urls', namespace='rest_framework'))
 )
-
-# handle 404s and 500s in development mode
-if settings.DEBUG:
-    urlpatterns += patterns(
-        '',
-        url(r'^500/$', 'django.views.generic.simple.direct_to_template', {
-            'template': '500.html'}),
-        url(r'^404/$', 'django.views.generic.simple.direct_to_template', {
-            'template': '404.html'}),
-    )
