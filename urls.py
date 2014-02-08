8b1e2530c465ddf592cb073c1ca0f964f6ed7a8d
Rob Charlwood <robcharlwood@gmail.com>
2014-02-08 15:12:06 +0000

    Sorted collected static directory and added rest framework auth urls

--- urls.py
+++ urls.py
@@ -1,8 +1,10 @@
 # import django deps
-from django.conf.urls import patterns, include
+from django.conf.urls import patterns, include, url
 
 # set up urls
 urlpatterns = patterns(
     '',
     (r'', include('core.urls')),
+    url(r'^api/auth/', include(
+        'rest_framework.urls', namespace='rest_framework'))
 )
