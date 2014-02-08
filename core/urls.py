493ea5b04baee89113263d7aad112bc4324612ee
Rob Charlwood <robcharlwood@gmail.com>
2014-02-08 19:38:11 +0000

    Bug fix

--- core/urls.py
+++ core/urls.py
@@ -2,13 +2,15 @@
 from django.conf.urls import url, patterns
 from django.conf import settings
 
+# urls
+urlpatterns = patterns('')
 
 # handle 404s and 500s in development mode
 if settings.DEBUG:
-    urlpatterns = patterns(
+    urlpatterns += patterns(
         '',
         url(r'^500/$', 'django.views.generic.simple.direct_to_template', {
-            'template': 'core/500.html'}),
+            'template': '500.html'}),
         url(r'^404/$', 'django.views.generic.simple.direct_to_template', {
-            'template': 'core/404.html'}),
+            'template': '404.html'}),
     )
