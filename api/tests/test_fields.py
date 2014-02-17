5c93472b6abf0d80b89ad7e0262ca4f7474c1445
Rob Charlwood <robcharlwood@gmail.com>
2014-02-17 22:00:45 +0000

    Added extra test for translation field to ensure that if a translation is present that it gets returned

--- api/tests/test_fields.py
+++ api/tests/test_fields.py
@@ -1,6 +1,7 @@
 # import django deps
 from django.test import TestCase
 from django.contrib.auth.models import User
+from django.utils import translation
 
 # import project deps
 from api.fields import TranslationField
@@ -17,8 +18,12 @@ class TranslationFieldTestCase(TestCase):
             title='My Blog Entry',
             content='# My Content',
             published=True)
+        self.trans = self.entry.translations.create(
+            language='es',
+            title='My Blog Entry es',
+            content='# My Content es')
 
-    def test_field_to_native_no_markdown(self):
+    def test_field_to_native_no_markdown_no_trans(self):
         """
             Make sure field_to_native()
             returns what we expect
@@ -27,7 +32,7 @@ class TranslationFieldTestCase(TestCase):
         result_1 = f.field_to_native(self.entry, 'content')
         self.assertEqual(u'# My Content', result_1)
 
-    def test_field_to_native_markdown(self):
+    def test_field_to_native_markdown_no_trans(self):
         """
             Make sure field_to_native()
             returns what we expect when markdown enabled
@@ -35,3 +40,26 @@ class TranslationFieldTestCase(TestCase):
         f = TranslationField(markdown=True)
         result_1 = f.field_to_native(self.entry, 'content')
         self.assertEqual(u'<h1>My Content</h1>', result_1)
+
+    def test_field_to_native_no_markdown_with_trans(self):
+        """
+            Make sure field_to_native()
+            returns what we expect when a translation is present
+        """
+        translation.activate('es')
+        f = TranslationField(markdown=False)
+        result_1 = f.field_to_native(self.entry, 'content')
+        self.assertEqual(u'# My Content es', result_1)
+        translation.activate('en')
+
+    def test_field_to_native_markdown_with_trans(self):
+        """
+            Make sure field_to_native()
+            returns what we expect when markdown enabled
+            and there is a translation present.
+        """
+        translation.activate('es')
+        f = TranslationField(markdown=True)
+        result_1 = f.field_to_native(self.entry, 'content')
+        self.assertEqual(u'<h1>My Content es</h1>', result_1)
+        translation.activate('en')
